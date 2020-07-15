a63bfb213aa1018c2f2f0eeaed65391f0ed54292
jkierem <juanmiguelgomez8@gmail.com>
2020-07-14 19:42:52 -0500

    Added route and stub component

--- src/containers/Router/index.js
+++ src/containers/Router/index.js
@@ -8,6 +8,7 @@ import Footer from '../Footer'
 import MovieModal from '../MovieModal'
 import TopsContainer from '../TopsContainer'
 import LoginModal from '../LoginModal'
+import Wishlists from '../Wishlists'
 
 const Route = ({ path, children }) => children
 
@@ -20,6 +21,7 @@ const Switch = ({ value, children }) => {
 
 const Router = () => {
     const current = usePathSelector("routing.current");
+    const auth = usePathSelector("user.authenticated");
     const [ movieId, setMovieId ] = useState('');
     const [ loginOpen, setLoginOpen ] = useState(false);
 
@@ -42,6 +44,9 @@ const Router = () => {
                 <MovieGrid onClickMovie={setMovieId}/>
                 <Footer />
             </Route>
+            {auth && <Route path="wishlists">
+                <Wishlists />
+            </Route>}
         </Switch>
         {movieId && <MovieModal id={movieId} open={movieId} onClose={handleClose}/>}
         {loginOpen === "anonymous" && <LoginModal open={loginOpen} onClose={handleLoginClose}/>}
